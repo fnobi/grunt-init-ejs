003e4a9429a4992b760b1da8ce613e8cbabad82e
Fujisawa Shin <fujisawa-shin@kayac.com>
2013-07-18 10:11:02 +0900

    rename等。

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,13 +1,20 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            html: {
+                files: ['src/ejs/*.ejs', 'src/ejs/**/*.ejs', 'src/option.dev.yaml'],
+                tasks: ['ejs']
+            }
+        },
         ejs: {
             template: ['src/ejs/*.ejs'],
             dest: './',
-            options: 'src/ejs-option.yaml'
+            options: 'src/option.dev.yaml'
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-simple-ejs');
 
     grunt.registerTask('build', ['ejs']);
